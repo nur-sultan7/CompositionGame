b007b938c7f205a3243b50e9033fe4fd3caba3bd
Nursultan <nur-sultan17@mail.ru>
2022-10-21 18:21:43 +0600

    impl data binding in GameFragment.kt

--- app/src/main/java/com/example/compositiongame/presentation/GameFragment.kt
+++ app/src/main/java/com/example/compositiongame/presentation/GameFragment.kt
@@ -1,21 +1,15 @@
 package com.example.compositiongame.presentation
 
-import android.content.res.ColorStateList
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import com.example.compositiongame.R
 import com.example.compositiongame.databinding.FragmentGameBinding
 import com.example.compositiongame.domain.entiteis.GameResult
-import com.example.compositiongame.domain.entiteis.Level
 
 
 class GameFragment : Fragment() {
@@ -34,18 +28,18 @@ class GameFragment : Fragment() {
             this, gameViewModelFactory
         )[GameViewModel::class.java]
     }
-    private val tvOptionsList: MutableList<TextView> by lazy {
-        mutableListOf<TextView>().apply {
-            with(binding) {
-                add(questionTvOption1)
-                add(questionTvOption2)
-                add(questionTvOption3)
-                add(questionTvOption4)
-                add(questionTvOption5)
-                add(questionTvOption6)
-            }
-        }
-    }
+//    private val tvOptionsList: MutableList<TextView> by lazy {
+//        mutableListOf<TextView>().apply {
+//            with(binding) {
+//                add(questionTvOption1)
+//                add(questionTvOption2)
+//                add(questionTvOption3)
+//                add(questionTvOption4)
+//                add(questionTvOption5)
+//                add(questionTvOption6)
+//            }
+//        }
+//    }
 
 //    @RequiresApi(33)
 //    override fun onCreate(savedInstanceState: Bundle?) {
@@ -62,60 +56,64 @@ class GameFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
         setViewModelObservers()
-        setOnClickListeners()
-
     }
 
     private fun setViewModelObservers() {
-        with(binding) {
-            with(viewModel) {
-                timerTime.observe(viewLifecycleOwner) {
-                    tvTimer.text = it
-                }
-                resultOfAnswers.observe(viewLifecycleOwner) {
-                    tvCountRightAnswers.text = it
-                }
-                percentOfRightAnswers.observe(viewLifecycleOwner) {
-                    pbQuestion.setProgress(it, true)
-                }
-                minPercent.observe(viewLifecycleOwner) {
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                        pbQuestion.secondaryProgress = it
-                    }
-                }
-                question.observe(viewLifecycleOwner) {
-                    tvVisibleNumber.text = it.visibleNumber.toString()
-                    tvQuestionSum.text = it.sum.toString()
-                    for (optionIndex in 0 until tvOptionsList.size) tvOptionsList[optionIndex].text =
-                        it.options[optionIndex].toString()
-                }
-                enoughRightAnswers.observe(viewLifecycleOwner) {
-                    if (it) tvCountRightAnswers.setTextColor(requireContext().getColor(R.color.green))
-                    else tvCountRightAnswers.setTextColor(requireContext().getColor(R.color.red))
-                }
-                enoughPercent.observe(viewLifecycleOwner) {
-                    if (it) pbQuestion.progressTintList =
-                        ColorStateList.valueOf(requireContext().getColor(R.color.green))
-                    else pbQuestion.progressTintList =
-                        ColorStateList.valueOf(requireContext().getColor(R.color.red))
-                }
-                gameResult.observe(viewLifecycleOwner) {
-                    launchGameFinishedFragment(it)
-                }
-            }
+        viewModel.gameResult.observe(viewLifecycleOwner){
+            launchGameFinishedFragment(it)
         }
+//        with(binding) {
+//            with(viewModel) {
+//                gameResult.observe(viewLifecycleOwner) {
+//                    launchGameFinishedFragment(it)
+//                }
+//                timerTime.observe(viewLifecycleOwner) {
+//                    tvTimer.text = it
+//                }
+//                resultOfAnswers.observe(viewLifecycleOwner) {
+//                    tvCountRightAnswers.text = it
+//                }
+//                percentOfRightAnswers.observe(viewLifecycleOwner) {
+//                    pbQuestion.setProgress(it, true)
+//                }
+//                minPercent.observe(viewLifecycleOwner) {
+//                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//                        pbQuestion.secondaryProgress = it
+//                    }
+//                }
+//                question.observe(viewLifecycleOwner) {
+//                    tvVisibleNumber.text = it.visibleNumber.toString()
+//                    tvQuestionSum.text = it.sum.toString()
+//                    for (optionIndex in 0 until tvOptionsList.size) tvOptionsList[optionIndex].text =
+//                        it.options[optionIndex].toString()
+//                }
+//                enoughRightAnswers.observe(viewLifecycleOwner) {
+//                    if (it) tvCountRightAnswers.setTextColor(requireContext().getColor(R.color.green))
+//                    else tvCountRightAnswers.setTextColor(requireContext().getColor(R.color.red))
+//                }
+//                enoughPercent.observe(viewLifecycleOwner) {
+//                    if (it) pbQuestion.progressTintList =
+//                        ColorStateList.valueOf(requireContext().getColor(R.color.green))
+//                    else pbQuestion.progressTintList =
+//                        ColorStateList.valueOf(requireContext().getColor(R.color.red))
+//                }
+//
+//            }
+//        }
     }
 
-    private fun setOnClickListeners() {
-        for (optionIndex in 0 until tvOptionsList.size) tvOptionsList[optionIndex].setOnClickListener {
-            setOnOptionClick(it)
-        }
-    }
+//    private fun setOnClickListeners() {
+//        for (optionIndex in 0 until tvOptionsList.size) tvOptionsList[optionIndex].setOnClickListener {
+//            setOnOptionClick(it)
+//        }
+//    }
 
-    private fun setOnOptionClick(option: View) {
-        viewModel.checkAnswer((option as TextView).text.toString().toInt())
-    }
+//    private fun setOnOptionClick(option: View) {
+//        viewModel.chooseAnswer((option as TextView).text.toString().toInt())
+//    }
 
     override fun onDestroyView() {
         super.onDestroyView()
@@ -144,16 +142,16 @@ class GameFragment : Fragment() {
 //        level = requireArguments().getParcelable<Level>(KEY_LEVEL) as Level
 //    }
 
-    companion object {
-        const val NAME = "GameFragment"
-        private const val KEY_LEVEL = "level"
-
-        fun newInstance(level: Level): GameFragment {
-            return GameFragment().apply {
-                arguments = Bundle().apply {
-                    putParcelable(KEY_LEVEL, level)
-                }
-            }
-        }
-    }
+//    companion object {
+//        const val NAME = "GameFragment"
+//        private const val KEY_LEVEL = "level"
+//
+//        fun newInstance(level: Level): GameFragment {
+//            return GameFragment().apply {
+//                arguments = Bundle().apply {
+//                    putParcelable(KEY_LEVEL, level)
+//                }
+//            }
+//        }
+//    }
 }
